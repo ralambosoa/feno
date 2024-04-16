b3b3adcc442153161d6bc867f10da4ac77858fcc
ralambosoa <96731310+ralambosoa@users.noreply.github.com>
2024-04-16 11:15:35 +0300

    code.php

--- code.php
+++ code.php
@@ -1,18 +1,7 @@
 <?php
 
-$directory = "./";
-
-// Obtenir la liste des fichiers et des répertoires dans le répertoire
-$files = scandir($directory);
-
-// Parcourir la liste des fichiers et répertoires
-foreach ($files as $file) {
-    // Ignorer les éléments spéciaux '.' et '..'
-    if ($file != '.' && $file != '..') {
-        // Afficher le nom de fichier ou de répertoire
-        echo $file . "<br>";
-    }
-}
-
+$chemin ='index.php';
+$contenu = file_get_contents ($chemin);
+ echo $contenu;
 
     ?>
